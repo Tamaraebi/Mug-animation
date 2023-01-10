640ab8d1ec911f46a069fd5b3da2f778982fbd4d
Tamara Ebi-ukuli <114423068+Tamaraebi@users.noreply.github.com>
2023-01-10 09:56:58 -0500

    Add files via upload

---
+++ animationproject.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector(".btn");
+const Container = document.querySelector(".lipton");
+
+btn.addEventListener("click", function () {
+   Container.classList.add("dip");
+});
+const button = document.querySelector(".btn");
+const Cont = document.querySelector(".mug2");
+
+button.addEventListener("click", function () {
+   Cont.classList.add("rotate");
+});
+const button2 = document.querySelector(".btn");
+const Cont2= document.querySelector(".mug3");
+
+button.addEventListener("click", function () {
+   Cont2.classList.add("rotate2");
+});
+
